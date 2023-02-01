0929337ed7289bc746d1786a3487337f609f94d9
marlleyck <marlleyck@hotmail.com>
2023-02-01 17:45:57 -0300

    refactor: delete hello endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,6 @@ import { getUsers } from "../controllers/usersController";
 
 const router = express.Router();
 
-router.get("/hello", (req, res) => {
-  return res.send({ msg: "Hello World" });
-});
-
 router.get("/users", getUsers);
 
 export default router;
