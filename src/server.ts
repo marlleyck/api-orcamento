1b54533dbd7bc4f8d1d5f7880e02c9cb04ea9a52
marlleyck <marlleyck@hotmail.com>
2023-02-04 21:32:39 -0300

    feat: add dotenv config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 
 import router from "./routes";
 
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
@@ -10,4 +14,4 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
 
-app.listen(3333, () => console.log("Server is running at port 3000..."));
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}...`));
