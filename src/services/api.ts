6a853adec7b153a763bbea20c8049b012c6fde07
marlleyck <marlleyck@hotmail.com>
2023-02-04 23:33:11 -0300

    feat: export baseURL api

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,5 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "https://mockend.com/juunegreiros/BE-test-api",
 });
+
+export const baseURL = "https://mockend.com/juunegreiros/BE-test-api";
